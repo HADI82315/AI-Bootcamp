812422fa19c84b38e716732d5a07b44308356e1d
Mohammad Hadi Sadeghian <hadisadeghian1382@gmail.com>
2025-10-18 22:23:01 +0330

    task 1 done

--- HW_L2_1/1.py
+++ HW_L2_1/1.py
@@ -3,10 +3,15 @@ while (1):
     user_input = input("please enter an integer greater than 3: ").replace(' ', '')
  
     if user_input.isdigit():
-        if int(user_input) > 3:
+        if (user_input := int(user_input)) > 3:
             break
         else:
             print("out of range number")
     else:
         print("invalid input")
-print("succesfull")
+
+numbers = range(1,user_input + 1)
+if user_input % 2:
+    print(*numbers[::2],sep=' ,')
+else:
+    print(*numbers[1::2],sep=' ,')
