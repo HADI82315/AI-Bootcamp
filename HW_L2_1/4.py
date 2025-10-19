f75e86d3bd9515c7e43e667d8ac8441a474e55c9
Mohammad Hadi Sadeghian <hadisadeghian1382@gmail.com>
2025-10-19 22:25:05 +0330

    getting grades added

--- HW_L2_1/4.py
+++ HW_L2_1/4.py
@@ -7,16 +7,33 @@ class OutOfRange(Exception):
         self.range = range
 
     def __str__(self):
-        return f"{self.value} out of {self.range},please try agian."
+        return f"{self.value} out of {self.range}!!"
+    
     
 while True:    
     try:
-        course_numbers = int(input("please enter number of courses: "))
-        if course_numbers < 1:
-            raise OutOfRange(value= course_numbers, range=(1,))
+        courses_number = int(input("please enter number of courses: "))
+        if courses_number < 1:
+            raise OutOfRange(value= courses_number, range=(1,))
     except ValueError:
-        print("number of courses shoud be integer")
+        print("number of courses must be integer!!")
     except OutOfRange as e:
         print(e)
     else:
         break  
+    
+grades = []
+while courses_number:
+    try:
+        grade = float(input(f"please enter {len(grades) + 1}th grade,must be in (0,20):"))
+        if not (0 <= grade <= 20):
+            raise OutOfRange(grade,(0,20))
+    except ValueError:
+        print("grade must be float!!")
+    except OutOfRange as e:
+        print(e)
+    else:
+        grades.append(grade)
+        courses_number -= 1 
+        
+av
