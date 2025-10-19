f8a0b3471e1cb24f10e7638080ddba2bc1c19f45
Mohammad Hadi Sadeghian <hadisadeghian1382@gmail.com>
2025-10-19 21:55:46 +0330

    out of range exception updated

--- HW_L2_1/1.py
+++ HW_L2_1/1.py
@@ -1,16 +1,17 @@
 class OutOfRange(Exception):
-    def __init__(self, value, *args):
+    def __init__(self, value, range=(), *args):
         super().__init__( *args)
         self.value = value
+        self.range = range
 
     def __str__(self):
-        return f"{self.value} is less than 3 and is invalid,please try agian"
+        return f"{self.value} out of {self.range},please try agian."
 
 while (1):
     try:
         user_input = int(input("please enter an integer greater than 3: "))
         if user_input < 3:
-            raise OutOfRange(user_input)
+            raise OutOfRange(user_input,range=(3,))
     except ValueError:
         print("input must be integer only,please try again")
     except OutOfRange as e:
