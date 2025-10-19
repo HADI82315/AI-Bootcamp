b490391fbb85398f2153fc59983205952f6c1622
Mohammad Hadi Sadeghian <hadisadeghian1382@gmail.com>
2025-10-19 21:43:30 +0330

    task done

--- HW_L2_1/3.py
+++ HW_L2_1/3.py
@@ -2,8 +2,20 @@ import re
 import string
 
 input_string = input("please enter a sentence: ").strip()
-punctuation = (f"[{re.escape(string.punctuation)}]+").replace('’', '')
+punctuation = f"[{re.escape('.,?:;!-(){}[]')}]+"
 
 clean_string =  re.sub(punctuation, ' ', input_string)
 
-print(clean_string)
+words = clean_string.split()
+result = {}
+
+while words:
+    key = words[0]
+    value = sum(1 for word in words if key.lower() == word.lower())
+    words = [word for word in words if key.lower() != word.lower()]    
+    result[key] = value
+
+print(result)
+    
+            
+            
