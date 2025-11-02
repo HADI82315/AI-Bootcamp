efb3d63291c1868edeff8b57a6fed8193026bbdb
Mohammad Hadi Sadeghian <hadisadeghian1382@gmail.com>
2025-11-03 00:27:44 +0330

    regex updated,uniqueness is added,best match finder completed

--- HW_L2_2/recommender.py
+++ HW_L2_2/recommender.py
@@ -25,19 +25,44 @@ def get_or_create_user(name):
     else:
         while True:
             input_movies = input("Enter your favorite movies (comma separated, 2 or 3 movies): ").strip()
-            pattern = r"^.+,.+,.+$"
-            if not re.match(pattern, movies):
+            pattern = r"^[^,\n]+,[^,\n]+(?:,[^,\n]+)?$"
+            if not re.match(pattern, input_movies):
                 print("Please enter 2 or 3 movies separated by commas.")
                 continue
             movies = [movie.strip() for movie in input_movies.split(',')]
-            if not (1 < len(movies) < 4):
-                print("Please enter 2 or 3 movies only.")
+            movies_number = len(set(movie.lower() for movie in movies))
+            if not (1 < movies_number < 4):
+                print("Please enter 2 or 3 unique movies only.")
                 continue
             break
             
-    users[name] = [movie.strip() for movie in movies.split(',')]
+    users[name] = movies
+
+def find_best_match(name, users):
+    common_numbers = {}
+    for user in users.keys():
+        if user == name:
+            continue
+        common_numbers[user] = similarity(name, user)
+        
+    common_numbers = dict(sorted(common_numbers.items(), reverse=True))
+    max = -1
+    for user, score in common_numbers.items():
+        if score > max:
+            max = score
+            best_match = user
+        
+    return best_match
+        
+def similarity(user_a, user_b):
+    movies_a = set(users[user_a])
+    movies_b = set(users[user_b])
+    common_movies = movies_a.intersection(movies_b)
+    return len(common_movies)
 
 users = load_users()
 name = input("Enter your name: ").strip()
 get_or_create_user(name)
+best_match_user = find_best_match(name, users)
+print(f"Your best match is: {best_match_user}")
 save_users(users)
